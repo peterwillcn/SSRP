d5004b640834ac0ead1b7ff89248f50f2dd15450
Ronald <ronald.fenelus1@gmail.com>
2011-06-02 12:47:47 -0400

    fixed nashtest.cpp

--- src/equilib/nashTest.cpp
+++ src/equilib/nashTest.cpp
@@ -8,13 +8,14 @@ bool nashEquilibrium(graphGroup& g)
     vector<path> final_paths = g.returnSharedPaths();
     vector<floatWInf> final_costs = g.returnSharedCosts();
 
-    if(debug)
-        output("searching for a nash equilibrium");
+    output("searching for a nash equilibrium");
     
     //loop through each journey
     //if a journey can improve start back at journey 0
     //if all journeys can not improve an equilibrium has been found
-    
+        
+  
+     
     
     int j = 0; 
     while(j < g.numJourneys()) 
@@ -26,39 +27,15 @@ bool nashEquilibrium(graphGroup& g)
         if( x < final_costs[j]) 
         {
             final_costs[j] = x;
-            if(debug)
-                output("journey " + str(j) + " has a better strategy");
+            output("journey " + str(j) + " has a better strategy");
             j = 0;
-            //output(spaces + "journey " + str(j) + " has no incentive to move.");
+            output("journey " + str(j) + " has no incentive to move.");
         }
         
         j++;
-        
-        /*
-        else 
-        {
-            result = false;
-            output(spaces + "journey " + str(j) + " will defect from the final solution.");
-            if(depth < nashDepth)
-            {
-              output(spaces + "Running test on resultant graph");
-              result = nashEquilibrium(g, depth + 1);
-            }
-            else
-            {
-              output(spaces + "could not find nash equilibrium after " + str(nashDepth) + " attempts.");
-            }
-            printJourney(g.getJourney(j));
-        }
-        
-        
-        g.removeJourney(j);
-        g.addJourney(j, final_paths[j]);
-        */
     }
 
-    if(debug)
-        output("This solution is a nash equilibrium");
+    output("This solution is a nash equilibrium");
     
     result = true;
     return result;
