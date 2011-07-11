2755514d716a62dcc961909c3083cac201c1db5b
Zeal Jagannatha <zfjagann@owu.edu>
2011-07-11 14:57:53 -0400

    Updated cluster scripts.

--- IIPegV.sh
+++ IIPegV.sh
@@ -1,10 +1,1 @@
-if [ "$1" == "00" ]
-then
-    sspp stat -v 50 -j 10 -w 5 50 -c 100 >> results00.txt &
-elif [ "$1" == "01" ]
-then
-    sspp stat -v 100 -j 20 -w 20 100 -c 100 >> results01.txt &
-elif [ "$1" == "02" ]
-then
-    sspp stat -v 200 -j 100 -w 20 100 -c 100 >> results02.txt &
-fi
+./sspp stat -v $1 -j $2 -w 20 100 -c 100 >> results-$1-$2-$3.txt &
--- runV.sh
+++ runV.sh
@@ -1,9 +1,19 @@
 rm -f compute-0-* results*
+NUMRUNS=10
 for node in compute-0-0 compute-0-1 compute-0-2 compute-0-3 compute-0-4 compute-0-5 compute-0-6 compute-0-7 compute-0-8 compute-0-9
 do
-  mpirun -np 1 --host $node sh IIPegV.sh 00 &
-  sleep 4
-  mpirun -np 1 --host $node sh IIPegV.sh 01 &
-  sleep 4
-  mpirun -np 1 --host $node sh IIPegV.sh 02 &
+  COUNTER=0
+  JOURNEYS=25
+  VERTICES=50
+  while [ -e "${node}-lock" ]; do
+    sleep 4
+  done
+  while [ $COUNTER -lt $NUMRUNS ]; do
+    touch "${node}-lock"
+    mpirun -np 1 --host $node sh IIPegV.sh $VERTICES $JOURNEYS $node
+    rm "${node}-lock"
+    let JOURNEYS=JOURNEYS+25
+    let VERTICES=VERTICES+50
+    let COUNTER=COUNTER+1
+  done 
 done
